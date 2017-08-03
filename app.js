7370fc1cedfb3ff1afdc1d0993fb5af62c2cbe02
Shireen Garcia <garciasal@gmail.com>
2017-08-03 15:43:41 -0700

    I need a mug with a mustache on it

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const mustacheExpress = require('mustache-express');
 const mongoose = require('mongoose');
 
 mongoose.Promise = require('bluebird');
-// Replace "test" with your database name.
 mongoose.connect('mongodb://localhost:27017/sbMugs');
 
 
