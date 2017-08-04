5b83494e83999ad69fdbc3481b685f5d13b26387
Shireen Garcia <garciasal@gmail.com>
2017-08-03 22:22:03 -0700

    Object show thyself

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 const mustacheExpress = require('mustache-express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/sbMugs');
@@ -13,10 +14,16 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.use(express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", function(req, res, next){
   res.render("index", {appType:"Starbucks Mugs"})
 })
+
+app.post('/', function(req, res){
+  console.log('Mug Info: ', req.body);
+  res.render("mugs", {id:req.body.id, city:req.body.city, country:req.body.country, edition:req.body.edition, image:req.body.image});
+});
 
 app.listen(3000, function(){
   console.log("App running on port 3000")
