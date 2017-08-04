83da14c1a7b9f56199cdade2c738da260b3863b5
Shireen Garcia <garciasal@gmail.com>
2017-08-04 15:52:17 -0700

    Trying to add to the database

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const mustacheExpress = require('mustache-express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost:27017/sbMugs');
-
-
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
@@ -20,10 +16,39 @@ app.get("/", function(req, res, next){
   res.render("index", {appType:"Starbucks Mugs"})
 })
 
-app.post('/', function(req, res){
+app.post('/mugs', function(req, res){
   console.log('Mug Info: ', req.body);
-  res.render("mugs", {id:req.body.id, city:req.body.city, country:req.body.country, edition:req.body.edition, image:req.body.image});
-});
+
+  var data = {
+    id:req.body.id,
+    city:req.body.city,
+    country:req.body.country,
+    edition:req.body.edition,
+    image:req.body.image
+  };
+
+  sbMugsModel.createsbMug(data, function(sbMugs) {
+    if (sbMugs) {
+      var model = {
+        appType: "Mug Added",
+        sbMugs: sbMugs
+      }
+      res.render("sbMugs", model);
+    }
+    else {
+      res.redirect("/");
+    }
+  });
+
+  res.render("mugs", {id:req.body.id,
+    city:req.body.city,
+    country:req.body.country,
+    edition:req.body.edition,
+    image:req.body.image});
+
+  });
+
+
 
 app.listen(3000, function(){
   console.log("App running on port 3000")
--- models/sbMugs.js
+++ models/sbMugs.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost:27017/sbMugs');
+
 const sbMugsSchema = new mongoose.Schema({
   id: {type: Number},
   city: {type: String},
@@ -9,5 +12,16 @@ const sbMugsSchema = new mongoose.Schema({
 })
 
 const sbMugs = mongoose.model('sbMugs', sbMugsSchema);
+
+// function getAllSmurfs(callback) {
+//   SmurfModel.findMany({})
+//     .then(function(smurfs) {
+//       callback(smurfs);
+//     })
+//     .catch(function(error) {
+//       console.log("error", error);
+//       callback(null);
+//     })
+// }
 
 module.exports = sbMugs;
